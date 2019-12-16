35da13dabb1fe65d79496fc231e6f6b9b065013c
steffaneribeiro <steffane.rib@gmai.com>
2019-12-16 15:39:48 -0300

    criacao de fragments

--- app/src/main/java/com/example/fragment/MainActivity.kt
+++ app/src/main/java/com/example/fragment/MainActivity.kt
@@ -8,5 +8,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentManeger = supportFragmentManager
+        val fragmentTransaction = fragmentManeger.beginTransaction()
+
+        val fragment = FragmentB()
+
+        fragmentTransaction.add(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 }
